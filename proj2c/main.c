103a191d0cc1ef16eceefe0dedd8e88fce09d6c1
Alice Martins Valero <104464133+Alimva@users.noreply.github.com>
2024-06-18 22:29:08 -0300

    Add files via upload

--- proj2c/main.c
+++ proj2c/main.c
@@ -1,547 +1,166 @@
-//Alice Martins Valero
-
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
-#define arv_tam 100
-#define stack_tam 100
+// Definindo os tokens
+#define PLUS '+'
+#define MINUS '-'
+#define TIMES '*'
+#define DIVIDE '/'
+#define LPAREN '('
+#define RPAREN ')'
+#define NUMBER 'n'
+#define END_OF_INPUT '\0'
 
-typedef struct{
-    char producoes[2];
-} Producoes;
+// Funções para o analisador sintático
+int expression();
+int term();
+int factor();
+void error(char *msg);
+void match(int token);
 
-char w[16];
-char output[2];
-int  len = 0;
-int c1 = 0;
-int c2 = 0;
-int c3 = 0;
-int c4 = 0;
+// Variáveis globais
+char *input;
+int index = 0;
+int parse_tree[100];
+int tree_index = 0;
+int tree_type = 2; // Define o tipo de árvore (2 = binária, 3 = ternária, 4 = quaternária, 5 = penta-ária)
 
-
-
-
-char stack[stack_tam];
-int top = -1;
-
-int pop() {
-    if (top >= 0) {
-        top--;
-    } else {
-        printf("Stack underflow!\n");
-    }
-    return 0;
+// Função para calcular o índice do filho na árvore n-ária
+int left_child_index(int i, int n) {
+    return n * i + 1;
 }
 
-int push(char symbol) {
-    if (top < stack_tam - 1) {
-        top++;
-        stack[top] = symbol;
-    } else {
-        printf("Stack overflow!\n");
-    }
-    return 0;
+int right_child_index(int i, int n) {
+    return n * i + n;
 }
 
-void S0(char token){
-    if(isdigit(token)){
-        output[0] = 'S';
-        output[1] = '5';
-    }
-    else if(token == '('){
-        output[0] = 'S';
-        output[1] = '4';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S1(char token){
-    if(token == '+'){
-        output[0] = 'S';
-        output[1] = '6';
-    }
-    else if(token == '$'){
-        output[0] = 'A';
-        output[1] = 'C';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S2(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '2';
-    }
-    else if(token == '*'){
-        output[0] = 'S';
-        output[1] = '7';
-    }
-    else if(token == '/'){
-        output[0] = 'S';
-        output[1] = '7';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '2';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '2';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S3(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '4';
-    }
-    else if(token == '*'){
-        output[0] = 'R';
-        output[1] = '4';
-    }
-    else if(token == '/'){
-        output[0] = 'R';
-        output[1] = '4';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '4';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '4';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S4(char token){
-    if(isdigit(token)){
-        output[0] = 'S';
-        output[1] = '5';
-    }
-    else if(token == '('){
-        output[0] = 'S';
-        output[1] = '4';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S5(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '6';
-    }
-    else if(token == '*'){
-        output[0] = 'R';
-        output[1] = '6';
-    }
-    else if(token == '/'){
-        output[0] = 'R';
-        output[1] = '6';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '6';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '6';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S6(char token){
-    if(isdigit(token)){
-        output[0] = 'S';
-        output[1] = '5';
-    }
-    else if(token == '('){
-        output[0] = 'S';
-        output[1] = '4';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S7(char token){
-    if(isdigit(token)){
-        output[0] = 'S';
-        output[1] = '5';
-    }
-    else if(token == '('){
-        output[0] = 'S';
-        output[1] = '4';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S8(char token){
-    if(token == '+'){
-        output[0] = 'S';
-        output[1] = '6';
-    }
-    else if(token == ')'){
-        push('S');
-        push('1');
-        push('1');
-        output[0] = 'S';
-        output[1] = '-';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S9(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '1';
-    }
-    else if(token == '*'){
-        output[0] = 'S';
-        output[1] = '7';
-    }
-    else if(token == '/'){
-        output[0] = 'S';
-        output[1] = '7';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '1';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '1';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S10(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '3';
-    }
-    else if(token == '*'){
-        output[0] = 'R';
-        output[1] = '3';
-    }
-    else if(token == '/'){
-        output[0] = 'R';
-        output[1] = '3';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '3';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '3';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void S11(char token){
-    if(token == '+'){
-        output[0] = 'R';
-        output[1] = '5';
-    }
-    else if(token == '*'){
-        output[0] = 'R';
-        output[1] = '5';
-    }
-    else if(token == '/'){
-        output[0] = 'R';
-        output[1] = '5';
-    }
-    else if(token == ')'){
-        output[0] = 'R';
-        output[1] = '5';
-    }
-    else if(token == '$'){
-        output[0] = 'R';
-        output[1] = '5';
-    }
-    else{
-        output[0] = 'B';
-        output[1] = 'R';
-    }
-}
-
-void action(char s, char token){
-    if(s == '0'){
-        S0(token);
-    }
-    else if(s == '1'){
-        S1(token);
-    }
-    else if(s == '2'){
-        S2(token);
-    }
-    else if(s == '3'){
-        S3(token);
-    }
-    else if(s == '4'){
-        S4(token);
-    }
-    else if(s == '5'){
-        S5(token);
-    }
-    else if(s == '6'){
-        S6(token);
-    }
-    else if(s == '7'){
-        S7(token);
-    }
-    else if(s == '8'){
-        S8(token);
-    }
-    else if(s == '9'){
-        S9(token);
-    }
-    else if(s == '_'){
-        S10(token);
-    }
-    else if(s == '-'){
-        S11(token);
-    }
-}
-
-void go_to(char s){
-    if(s == '0'){
-        if(c1 == 0){
-            c1 = 1;
-            push('F');
-            push('S');
-            push('3');
-        }
-        else if(c1 == 1){
-            c1 = 2;
-            push('T');
-            push('S');
-            push('2');
-        }
-        else if(c1 == 2){
-            c1 = 3;
-            push('E');
-            push('S');
-            push('1');
-        }
-        else{
-            pop();
-            pop();
-            pop();
-            if(s == '_' || s == '-'){
-                pop();
-            }
-        }
-    }
-    else if(s == '4'){
-        if(c2 == 0){
-            c2 = 1;
-            push('F');
-            push('S');
-            push('3');
-        }
-        else if(c2 == 1){
-            c2 = 2;
-            push('T');
-            push('S');
-            push('2');
-        }
-        else if(c2 == 2){
-            c2 = 3;
-            push('E');
-            push('S');
-            push('8');
-        }
-        else{
-            pop();
-            pop();
-            pop();
-            if(s == '_' || s == '-'){
-                pop();
-            }
-        }
-    }
-    else if(s == '6'){
-        if(c3 == 0){
-            c3 = 1;
-            push('F');
-            push('S');
-            push('3');
-        }
-        else if(c3 == 1){
-            c3 = 2;
-            push('T');
-            push('S');
-            push('9');
-        }
-        else{
-            pop();
-            pop();
-            pop();
-            if(s == '_' || s == '-'){
-                pop();
-            }
-        }
-    }
-    else if(s == '7'){
-        if(c4 == 0){
-            c4 = 1;
-            push('F');
-            push('S');
-            push('_');
-        }
-        else{
-            pop();
-            pop();
-            pop();
-            if(s == '_' || s == '-'){
-                pop();
-            }
-        }
-    }
-    else {
-        pop();
-        pop();
-        pop();
-        if(s == '_' || s == '-'){
-            pop();
-        }
-    }
+int nth_child_index(int i, int n, int nth) {
+    return n * i + nth;
 }
 
 int main() {
-    FILE *file;
-    int cont = 0;
-    char arv[arv_tam];
-    Producoes p[15];
+    char buffer[100];
 
-    // Open file
-    file = fopen("./input.txt", "r");
-    if (file == NULL) {
-        printf("erro ao abrir o arquivo\n");
-        return 1;
+    printf("Digite a expressão aritmética (use espaço para separar): ");
+    fgets(buffer, sizeof(buffer), stdin);
+
+    // Remover o caractere de nova linha do final de buffer
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    // Definir o input para o analisador
+    input = buffer;
+
+    // Iniciar análise sintática
+    if (expression() && input[index] == END_OF_INPUT) {
+        printf("A expressão foi aceita sintaticamente.\n");
+
+        // Imprimir a árvore de análise sintática (representada como um vetor de inteiros)
+        printf("Árvore de análise sintática (código numérico):\n");
+        for (int i = 0; i < tree_index; i++) {
+            printf("%d ", parse_tree[i]);
+        }
+        printf("\n");
+
+    } else {
+        error("Erro sintático.");
     }
 
-    printf("\nEstado   |   Palavra   |   Acao\n"); //d do delta
-    printf("\n======== ===============  ======\n");
+    return 0;
+}
 
-    
+// Função para analisar uma expressão
+int expression() {
+    int result = term();
 
-//inicializa a pilha
-
-    while(!feof(file)){
-        cont = 0; 
-        push('S');
-        push('0');
-        fscanf(file, "%s", w);
-        for(int i = 0; i < strlen(w);i++){
-
-            for(int j = 0; j < top + 1; j++){
-                printf("%c ",stack[j]);
-            }
-            printf(" | ");
-            for(int k = i; k < strlen(w);k++){
-                printf("%c ",w[k]);
-            }
-            action(stack[top],w[i]);
+    while (input[index] == PLUS || input[index] == MINUS) {
+        char op = input[index++];
+        int term_value = term();
         
-            if(output[1] == '_'){
-                printf("|   10  \n");
-            }
-            else if(output[1] == '-'){
-                printf("|  11  \n");
-            }
-            else{
-                printf("|  %c %c  \n",output[0],output[1]);
-            }
-            p[cont].producoes[0] = output[0];
-            p[cont].producoes[1] = output[1];
-            ++cont;
-        //d2| p2
-            if(output[0] == 'S'){
-                push(w[i]);
-                push(output[0]);
-                push(output[1]);
-            }
-            else if(output[0] == 'R'){
-                pop();
-                pop();
-                pop();
-                go_to(stack[top]);
-                i--;
-            }
-            else if(output[0] == 'B'){
-                printf("Palavra nao reconhecida\n\n");
-                break;
-            }
-            else if(output[0] == 'A'){
-                printf("Palavra reconhecida\n\n");
-                break;
-            }
-        }
-        for(int i = -1; i <= top; i++){
-            pop();
-        }
-        arv[0] = 'S';
-
-    for(int j=0; j<cont; j++){
-        //armazena na arvore aSb
-        if(p[j].producoes[0] == 'p' && p[j].producoes[1] == '1'){
-            arv[3*j+1] = 'a';
-            arv[3*j+2] = 'S';
-            arv[3*j+3] = 'b';
-        } else if(p[j].producoes[0] == 'p' && p[j].producoes[1] == '2'){ //armazena o c, ultima folha da arvores
-            arv[3*j+1] = 'c';
-        }
-    }
-
-    //print da arvore
-    printf("\n\n=== ARVORE DE ANALISE SINTATICA ===\n\n");
-
-    printf("   %c   ", arv[0]); printf("\n / | \\"); //imprime a raiz
-
-    for(int j=0; j<cont; j++){
-        if(arv[3*j+1] == 'c'){
-            printf("\n   %c   ", arv[3*j+1]); //imprime a ultima folha
-        }else{
-            printf("\n%c  %c  %c", arv[3*j+1], arv[3*j+2], arv[3*j+3]); printf("\n / | \\"); //imprime as demais folhas
-        }
-    }
-    printf("\n\n");
+        // Construir a árvore de análise
+        parse_tree[tree_index] = op;
+        parse_tree[left_child_index(tree_index, tree_type)] = result;
+        parse_tree[right_child_index(tree_index, tree_type)] = term_value;
+        tree_index++;
         
+        if (op == PLUS) {
+            result += term_value;
+        } else {
+            result -= term_value;
+        }
+    }
+
+    return result;
+}
+
+// Função para analisar um termo
+int term() {
+    int result = factor();
+
+    while (input[index] == TIMES || input[index] == DIVIDE) {
+        char op = input[index++];
+        int factor_value = factor();
+        
+        // Construir a árvore de análise
+        parse_tree[tree_index] = op;
+        parse_tree[left_child_index(tree_index, tree_type)] = result;
+        parse_tree[right_child_index(tree_index, tree_type)] = factor_value;
+        tree_index++;
+
+        if (op == TIMES) {
+            result *= factor_value;
+        } else {
+            if (factor_value == 0) {
+                error("Divisão por zero.");
+            }
+            result /= factor_value;
+        }
+    }
+
+    return result;
+}
+
+// Função para analisar um fator
+int factor() {
+    int result;
+
+    if (isdigit(input[index])) {
+        // Se for um número, converter para inteiro
+        result = input[index++] - '0';
+        while (isdigit(input[index])) {
+            result = result * 10 + (input[index++] - '0');
+        }
+
+        // Construir a árvore de análise
+        parse_tree[tree_index] = NUMBER;
+        parse_tree[left_child_index(tree_index, tree_type)] = result;
+        parse_tree[right_child_index(tree_index, tree_type)] = -1; // Indicador de folha
+        tree_index++;
+
+    } else if (input[index] == LPAREN) {
+        // Se for um parêntese esquerdo, analisar a expressão dentro dos parênteses
+        index++;
+        result = expression();
+        match(RPAREN);
+
+    } else {
+        error("Erro: esperado número ou '('.");
+    }
+
+    return result;
+}
+
+// Função para emitir mensagens de erro
+void error(char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(EXIT_FAILURE);
+}
+
+// Função para verificar e consumir um token esperado
+void match(int token) {
+    if (input[index] == token) {
+        index++;
+    } else {
+        error("Erro de correspondência de token.");
     }
 }
